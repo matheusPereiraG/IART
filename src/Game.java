38a8ed75648dd575522134f26beecde64b7f5972
Ricardo Cardoso <up201604686@fe.up.pt>
2020-03-26 21:38:38 +0000

    Added levels

--- src/Game.java
+++ src/Game.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 public class Game {
     private static ArrayList<Level> levels;
     private static int currLevel;
-    private static int NUM_LEVELS = 1;
+    private static int NUM_LEVELS = 4;
     private static boolean DEBBUG = true;
 
     Game() {
