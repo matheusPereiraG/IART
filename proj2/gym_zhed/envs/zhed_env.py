6d4b141165e1ccdcf4a096049a1ff66a3b3267e6
shakazulo <matheus.pgon1@gmail.com>
2020-05-20 17:40:35 +0100

    init action_space and observation_space

--- proj2/gym_zhed/envs/zhed_env.py
+++ proj2/gym_zhed/envs/zhed_env.py
@@ -8,9 +8,18 @@ class ZhedEnv(gym.Env):
   metadata = {'render.modes': ['human']}
 
   def __init__(self, m, n, pieces, terminalState):
-    self.grid = np.zeros((m,n))
+    self.board = np.zeros((m,n))
     self.m = m
     self.n = n
+    self.done = False
+    self.pieces = pieces
+    #for each piece 4 different movements thus being a multidiscrete space
+    aux_array = np.full(len(pieces), 4)
+    self.action_space = spaces.MultiDiscrete(aux_array)
+    print(self.action_space.sample())
+    self.observation_space
+    self.addPieces()
+    #self.action_space
 
   def step(self, action):
     ...
@@ -21,6 +30,13 @@ class ZhedEnv(gym.Env):
   def close(self):
     ...
 
+  def addPieces(self):
+    ...
+  
+  def calculateActionSpace(self):
+    pass
+    
+
 if __name__== "__main__":
-  ZhedEnv(10,10,[[1,2,3], [1,4,4]], [10,10])
-  print("bacalhau")
+  pieces = [[2,2,4], [2,4,5]] #level 4 of original zhed game
+  ZhedEnv(8,8,pieces,[4,2])
