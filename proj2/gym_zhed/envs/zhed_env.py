ee0e3d8a4b2c7ab47e15976b863d60dedc4e14d9
Ricardo Cardoso <up201604686@fe.up.pt>
2020-05-26 23:42:55 +0100

    q-learning almost done

--- proj2/gym_zhed/envs/zhed_env.py
+++ proj2/gym_zhed/envs/zhed_env.py
@@ -2,6 +2,7 @@ import gym
 from gym import error, spaces, utils
 from gym.utils import seeding
 import numpy as np
+from numpy import random
 import matplotlib.pyplot as plt
 import math 
 import os
@@ -9,7 +10,10 @@ import os
 class ZhedEnv(gym.Env):
   metadata = {'render.modes': ['human']}
 
+  levels = ["level1.txt", "level2.txt", "level3.txt", "level4.txt", "level5.txt", "level6.txt", "level7.txt"]
+
   def __init__(self, filename):
+    super().__init__()
     path = os.getcwd()
     os.path.abspath(os.path.join(path, filename))
     self.filename = filename
@@ -22,7 +26,7 @@ class ZhedEnv(gym.Env):
     self.action_space = spaces.Discrete(len(self.pieces) *4) #[0,1,2,3,4,5,6,7]
     self.possible_moves = np.full(self.action_space.n,True)
     print(self.possible_moves)
-    self.observation_space = spaces.Discrete(4^len(self.pieces))
+    self.observation_space = spaces.Discrete(2^(self.r*self.c))
     #self.observation_space = spaces.Box(0, 1, [self.r,self.c],dtype=np.int) #spaces.Box(0,len(self.pieces))
     #self.observation_space.sample()
 
@@ -32,7 +36,8 @@ class ZhedEnv(gym.Env):
     # verifica se a acao é possivel
     if self.possible_moves[action] == False:
       print("impossible action (", action, ")" ,sep="")
-      return #state, reward, done, {}
+      print("encode: ", self.encode())
+      return self.encode(), -100, False, {}
 
     # obtem a peça e a direção pretendidos
     pieceIndex = action // 4
@@ -54,22 +59,37 @@ class ZhedEnv(gym.Env):
     print("pieces: ", self.pieces)
     print("diffExpansion: ", diffExpansion)
 
-    #return observation, reward, done, {}
+    if(self.done):
+      reward = 100
+    else:
+      reward = diffExpansion * 10
+
+    print("encode: ", self.encode())
+    return self.encode(), reward, self.done, {}
 
   def reset(self):
-    self.__init__(self.filename)
+    self.__init__(self.levels[random.randint(7)])
 
-  def render(self, mode='human'):
+  def render(self, mode='human', close=False):
     print(np.matrix(self.board))
 
   def close(self):
     ...
 
-  def addPieces(self):
-    ...
   
-  def calculateActionSpace(self):
-    ...
+  def encode(self):
+    code = 0
+    num = 0
+    for i in range(self.r):
+      for j in range(self.c):
+        if(self.board[i,j] == '.'):
+          temp = 0
+        else:
+          temp = 1
+        code = code | (temp << num)
+        num += 1
+    return code
+  
     
   def getpieces(self):
     pieces = np.array([[0,0,0,False]])
@@ -92,7 +112,6 @@ class ZhedEnv(gym.Env):
     return func(piece)
   
   def down(self,piece):
-    print("down")
     numCellsExpanded = 0
     x = int(piece[2])
     y = int(piece[1])
@@ -117,7 +136,6 @@ class ZhedEnv(gym.Env):
     return numCellsExpanded - power
 
   def up(self,piece):
-    print("up")
     numCellsExpanded = 0
     x = int(piece[2])
     y = int(piece[1])
@@ -142,7 +160,6 @@ class ZhedEnv(gym.Env):
     return numCellsExpanded - power
 
   def right(self,piece):
-    print("right")
     numCellsExpanded = 0
     x = int(piece[2])
     y = int(piece[1])
@@ -167,7 +184,6 @@ class ZhedEnv(gym.Env):
     return numCellsExpanded - power
 
   def left(self,piece):
-    print("left")
     numCellsExpanded = 0
     x = int(piece[2])
     y = int(piece[1])
--- proj2/test.py
+++ proj2/test.py
@@ -1,28 +1,59 @@
 import gym
 import numpy as np 
+from IPython.display import clear_output
+from time import sleep
 
+def print_frames(frames):
+    for i, frame in enumerate(frames):
+        clear_output(wait=True)
+        #print(frame['frame'].getvalue())
+        print(f"Timestep: {i + 1}")
+        print(f"State: {frame['state']}")
+        print(f"Action: {frame['action']}")
+        print(f"Reward: {frame['reward']}")
+        sleep(.1)
 
 
 env = gym.make("gym_zhed:zhed-v0")
-
+print(env.observation_space.n)
 q_table = np.zeros([env.observation_space.n, env.action_space.n])
 
 print("q_table:")
 print(q_table)
 
 
-"""
-print("obs_space ", env.observation_space)
-print("obs_space.low ", env.observation_space.low)
-print("obs_space.high ", env.observation_space.high)
-print("obs_space.sample ", env.observation_space.sample())
-"""
+state = env.encode()
+print("state: ", state)
+
+steps = 0
+penalties, reward = 0,0
+
+frames = []
+
+done = False
 
 
-while not env.done:
-    if env.hasMovesLeft():
-        env.step(env.action_space.sample())
-        env.render()
-      
-    else:
+while not done:
+    action = env.action_space.sample()
+    state, reward, done, info = env.step(action)
+
+    if reward < 0:
+        penalties += reward/10
+
+    frames.append({
+        'frame': env.render(),
+        'state': state,
+        'action': action,
+        'reward': reward
+    })
+
+    steps += 1
+
+    if not env.hasMovesLeft():
         env.reset()
+        
+print("Timesteps taken: {}".format(steps))
+print("Penalties incurred: {}".format(penalties))
+
+print_frames(frames)
+
