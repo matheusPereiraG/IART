3cb589a02ceeb4078585fb3905e6e68a57211ad8
Ricardo Cardoso <up201604686@fe.up.pt>
2020-05-26 22:05:02 +0100

    minor changes

--- proj2/test.py
+++ proj2/test.py
@@ -1,14 +1,24 @@
 import gym
+import numpy as np 
+
+
 
 env = gym.make("gym_zhed:zhed-v0")
 
-print(env.observation_space)
-print(env.observation_space.low)
-print(env.observation_space.high)
-print(env.observation_space.sample())
+q_table = np.zeros([env.observation_space.n, env.action_space.n])
 
-#for i in range(40):
+print("q_table:")
+print(q_table)
+
+
 """
+print("obs_space ", env.observation_space)
+print("obs_space.low ", env.observation_space.low)
+print("obs_space.high ", env.observation_space.high)
+print("obs_space.sample ", env.observation_space.sample())
+"""
+
+
 while not env.done:
     if env.hasMovesLeft():
         env.step(env.action_space.sample())
@@ -16,4 +26,3 @@ while not env.done:
       
     else:
         env.reset()
-"""
